ac7dc897ad961a953c9cd9439f1bda1a1d2cf80d
liquanlong <812946952@qq.com>
2018-01-30 22:21:43 +0800

    Create plotData.m

---
+++ week2/plotData.m
@@ -0,0 +1,8 @@
+function plotData(x,y)
+
+  figure; % open a new figure window 
+
+  plot(x,y,'rx','MarkerSize',10);
+  xlabel('Population of city in 10,000s');
+  ylabel('profile in $10,000s');
+end
