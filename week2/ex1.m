5b2068202ef57028706544bbb700a77df469a9bb
liquanlong <812946952@qq.com>
2018-01-30 22:18:05 +0800

    Create ex1.m

---
+++ week2/ex1.m
@@ -0,0 +1,14 @@
+%% Initialization
+clear ; close all; clc
+%% ======================= Part 1: Plotting =======================
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+
+% Plot Data
+plotData(X, y);
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
